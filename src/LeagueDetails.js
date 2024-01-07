d4875aa2f74ca68d6d0561af415b680c6b7f96c2
dgatanis <122374677+dgatanis@users.noreply.github.com>
2024-01-07 09:41:36 -0700

    asdf

--- src/LeagueDetails.js
+++ src/LeagueDetails.js
@@ -1,24 +1,14 @@
-async function myFunction(){
-  const rosterResponse = await fetch(`https://api.sleeper.app/v1/league/1046222222567784448/rosters`, {compress: true}).catch((err) => { console.error(err); });
-  const rosters = await rosterResponse.json();
-  console.log(rosters);
-  return 
-}
 async function getLeagueData() { 
   const response = await fetch(`https://api.sleeper.app/v1/league/1046222222567784448/users`);
   const data = await response.json();
   const users = data.map((user) => user.user_id);
   const displayNames = data.map((user) => user.metadata.team_name);
   const usersElement = document.getElementById("myUsers");
-  // for (let userId of users) {
-  //     getRosterForUser();
-  // }
-  //usersElement.textContent = 'User: ' + displayNames;
   return 
 }
 
-async function getRosters(){
-  const userResponse = await fetch(`https://api.sleeper.app/v1/league/1046222222567784448/rosters`); 
+async function getRostersForLeague(leagueId){
+  const userResponse = await fetch(`https://api.sleeper.app/v1/league/${leagueId}/rosters`); 
   const userData = await userResponse.json(); 
   const usersElement = document.getElementById("myUsers");
   for (let users of userData) {
