7117980c9a4babfc53a26d849a8ebab797eb35ed
dgatanis <122374677+dgatanis@users.noreply.github.com>
2024-01-07 10:16:29 -0700

    fg

--- src/js/Owners.js
+++ src/js/Owners.js
@@ -1,7 +1,18 @@
-async function getOwnersForLeague(leagueId) { 
+async function getTeamNamesForLeague(leagueId) { 
     const usersResponse = await fetch(`https://api.sleeper.app/v1/league/${leagueId}/users`);
     const usersData = await usersResponse.json();
     const displayNames = usersData.map((user) => user.metadata.team_name);
-    console.log(displayNames);
-    return 
-  }
+    return displayNames;
+}
+
+async function getOwnerAvatarForLeague(leagueId) { 
+    const usersResponse = await fetch(`https://api.sleeper.app/v1/league/${leagueId}/users`);
+    const usersData = await usersResponse.json();
+    const avatarURL = usersData.map((user) => user.metadata.avatar);
+    for (let avatar of avatarURL)
+    {
+        var img = document.createElement("img");
+        img.setAttribute('src', avatar);
+        document.body.appendChild(img);
+    }
+}
