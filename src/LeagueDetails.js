0e341867fc4931a79e54eb1b0005eadb1a0c7ab2
dgatanis <122374677+dgatanis@users.noreply.github.com>
2024-01-06 22:25:36 -0700

    asdf

--- src/LeagueDetails.js
+++ src/LeagueDetails.js
@@ -7,14 +7,23 @@ async function myFunction(){
 async function getLeagueData() { 
   const response = await fetch(`https://api.sleeper.app/v1/league/1046222222567784448/users`);
   const data = await response.json();
+  const users = data.map((user) => user.user_id);
   const displayNames = data.map((user) => user.metadata.team_name);
   const usersElement = document.getElementById("myUsers");
-  usersElement.textContent = 'User: ' + displayNames;
+  for (let userId of users) {
+    for (let team of displayNames) {
+      usersElement.append('User: ' + team);
+      getRosterForUser(userId);
+    }
+  }
+  //usersElement.textContent = 'User: ' + displayNames;
   return 
 }
 
-async function getUser(userId){
-  const userResponse = await fetch(`https://api.sleeper.app/v1/user/${userId}`); 
+async function getRosterForUser(userId){
+  const userResponse = await fetch(`https://api.sleeper.app/v1/league/1046222222567784448/rosters`); 
   const userData = await userResponse.json(); 
-  return { ...user, display_name: userData.display_name }; 
+  const usersElement = document.getElementById("myUsers");
+  usersElement.append("test");
+  //return
 }
