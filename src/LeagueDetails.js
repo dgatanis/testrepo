6841375434996272960249c487601dad8a59ad04
dgatanis <122374677+dgatanis@users.noreply.github.com>
2024-01-07 09:49:21 -0700

    afd

--- src/LeagueDetails.js
+++ src/LeagueDetails.js
@@ -8,10 +8,11 @@ async function getLeagueData() {
 }
 
 async function getRostersForLeague(leagueId){
-  const userResponse = await fetch(`https://api.sleeper.app/v1/league/${leagueId}/rosters`); 
-  const userData = await userResponse.json(); 
+  const rosterResponse = await fetch(`https://api.sleeper.app/v1/league/${leagueId}/rosters`); 
+  const rosterData = await rosterResponse.json(); 
   const usersElement = document.getElementById("myUsers");
-  for (let users of userData) {
+  for (let users of rosterData) {
     usersElement.append(users.players + '\n');
   }
+  console.log(rosterData);
 }
