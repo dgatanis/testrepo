6325a4cfd2f05bdb4879ebdbbdbce08cbe71eccf
dgatanis <122374677+dgatanis@users.noreply.github.com>
2024-01-06 21:53:19 -0700

    asdf

--- src/LeagueRosters.js
+++ src/LeagueRosters.js
@@ -5,21 +5,14 @@ async function myFunction(){
   return 
 }
 async function getLeagueData() { 
-  const response = await fetch(`https://api.sleeper.app/v1/league/1046222222567784448/users`); 
-  const users = await response.json(); const matchupsResponse = await fetch(`https://api.sleeper.app/v1/league/1046222222567784448/matchups/4`); 
-  const matchups = await matchupsResponse.json(); const usersByMatchup = matchups.map(matchup => { 
-      const userIDs = matchup.users; 
-      const matchupUsers = userIDs.map(userID => { 
-          const user = users.find(user => user.user_id === userID); 
-          getUser(user.user_id);
-      }); 
-      return matchupUsers; 
-  }); 
-  return { usersByMatchup, matchups }; 
+  const response = await fetch(`https://api.sleeper.app/v1/league/1046222222567784448/users`);
+  const data = await response.json();
+  const displayNames = data.map((user) => user.display_name);
+  return displayNames.join(", ");
 }
 
 async function getUser(userId){
-  const userResponse = await fetch(`https://api.sleeper.app/v1/user/${user.user_id}`); 
+  const userResponse = await fetch(`https://api.sleeper.app/v1/user/${userId}`); 
   const userData = await userResponse.json(); 
   return { ...user, display_name: userData.display_name }; 
 }
