cfb472970563714d68052e7ed92311ba514b0732
dgatanis <122374677+dgatanis@users.noreply.github.com>
2024-01-06 20:33:50 -0700

    dsafsdaf

--- src/LeagueRosters.js
+++ src/LeagueRosters.js
@@ -1,4 +1,4 @@
-function myFunction(){
+async function myFunction(){
   const myTest = await fetch(`https://api.sleeper.app/v1/league/1046222222567784448/rosters`, {compress: true}).catch((err) => { console.error(err); });
   console.log(myTest.toString());
   return 
