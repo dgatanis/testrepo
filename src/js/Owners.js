a3126d25dbb3996ab8bb72568b04425a82db77ee
dgatanis <122374677+dgatanis@users.noreply.github.com>
2024-01-15 11:24:36 -0700

    asdf

--- src/js/Owners.js
+++ src/js/Owners.js
@@ -17,20 +17,46 @@ async function getTeamNamesForLeague(leagueId,userid=-1) {
                 var powerRanking = document.getElementById("PowerRanking1");
                 powerRanking.innerHTML=user.metadata.team_name
             }
+            else
+            {
+                console.log("error loading team name");
+            }
         }
         return "displayNames";
     }
     
 }
 
-async function getOwnerAvatarForLeague(leagueId) { 
+async function getOwnerAvatarForLeague(leagueId,userid=-1) { 
     const usersResponse = await fetch(`https://api.sleeper.app/v1/league/${leagueId}/users`);
     const usersData = await usersResponse.json();
-    const avatarURL = usersData.map((user) => user.metadata.avatar);
-    for (let avatar of avatarURL)
-    {
-        var img = document.createElement("img");
-        img.setAttribute('src', avatar);
-        document.body.appendChild(img);
+
+    if(userid==-1)
+    {    
+        const avatarURL = usersData.map((user) => user.metadata.avatar);
+        for (let avatar of avatarURL)
+        {
+            var img = document.createElement("img");
+            img.setAttribute('src', avatar);
+            document.body.appendChild(img);
+        }
     }
+    else
+    {
+        const users = usersData.map((user) => user);
+        for (let user of users)
+        {
+            if(user.user_id==userid)
+            {
+                var powerRanking = document.getElementById("PowerRanking1");
+                const avatarURL = user.metadata.avatar;
+                var img = document.createElement("img");
+                img.setAttribute('src', avatar);
+                powerRanking.innerHTML+=img
+            }
+
+        }
+    }
+
+
 }
