582c02f17dd04b0df10c20986fafba07c4b930e7
dgatanis <122374677+dgatanis@users.noreply.github.com>
2024-01-22 21:02:58 -0700

    asdf

--- src/js/Players.js
+++ src/js/Players.js
@@ -1,8 +1,8 @@
-async function getPlayers() {
-    //const playersResponse = await fetch(`https://api.sleeper.app/v1/players/nfl`); 
-    //const playersData = await playersResponse.json(); 
+export default async function getPlayers() {
+    const playersResponse = await fetch(`https://api.sleeper.app/v1/players/nfl`); 
+    const playersData = await playersResponse.json(); 
 
-    //let str = JSON.stringify(playersData);
+    let str = JSON.stringify(playersData);
     /*
     Need to create a new js file to export the players data to and store it. 
     Use something like:
@@ -10,7 +10,7 @@ async function getPlayers() {
     And then to import the file into this js file use:
         import { playerData } from './path/to/file.js';
     
-
+    */
     const fs = require('fs');
 
     // Convert the JSON object to a string
@@ -24,5 +24,5 @@ async function getPlayers() {
     }
         console.log('File has been written successfully!');
     });
-    */
+    
 }
