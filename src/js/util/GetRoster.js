c5eaceee84fbe29fb0ff9e181a994f06cdad4b49
dgatanis <122374677+dgatanis@users.noreply.github.com>
2024-01-24 16:31:51 -0700

    asdf

--- src/js/util/GetRoster.js
+++ src/js/util/GetRoster.js
@@ -1,2 +1,10 @@
 import getPlayers from '../util/GetPlayers.js';
-localStorage.setItem("playersData", getPlayers());
+if(localStorage.getItem("playersData"))
+{
+    console.log("already here")}
+else
+{
+    localStorage.setItem("playersData", getPlayers());
+}
+
+console.log(JSON.parse(localStorage.getItem("playersData")));
