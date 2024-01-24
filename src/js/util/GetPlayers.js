faa05fad5f04f41ed0b059cdd31210961809a3b5
dgatanis <122374677+dgatanis@users.noreply.github.com>
2024-01-23 19:33:28 -0700

    asdf

--- src/js/util/GetPlayers.js
+++ src/js/util/GetPlayers.js
@@ -1,7 +1,10 @@
 export default async function getPlayers() {
     const playersResponse = await fetch(`https://api.sleeper.app/v1/players/nfl`); 
-    const playersData = await playersResponse.json(); 
+    if(playersResponse.ok)
+    {
+        const playersData = await playersResponse.json(); 
+        var jsonString = JSON.stringify(playersData);
+        return jsonString;
+    }
     // Convert the JSON object to a string
-    const jsonString = JSON.stringify(playersData);
-    return jsonString;
 }
