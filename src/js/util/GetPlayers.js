9b726c9478debe241b734801d33d1426732bb8d1
dgatanis <122374677+dgatanis@users.noreply.github.com>
2024-01-24 21:02:51 -0700

    asdf

--- src/js/util/GetPlayers.js
+++ src/js/util/GetPlayers.js
@@ -1,4 +1,17 @@
-export default async function getPlayers() {
-    //const playersResponse = await fetch(`https://api.sleeper.app/v1/players/nfl`, {compress: true}); 
-    // Convert the JSON object to a string
+async function getPlayers() {
+    let playersInfo = null;
+
+    if(typeof window !== 'undefined') {
+        playersInfo = JSON.parse(localStorage.getItem("playersInfo"));
+    }
+    
+    if(!playersInfo) {
+    
+        const res  = await fetch(`https://api.sleeper.app/v1/players/nfl`, {compress: true}); 
+        const data = await res.json();
+        if(typeof window !== 'undefined') {
+            localStorage.setItem("playersInfo", JSON.stringify(data))
+        }
+    
+    }
 }
--- src/js/util/GetRoster.js
+++ src/js/util/GetRoster.js
@@ -1,21 +1,7 @@
 //import getPlayers from '../util/GetPlayers.js';
-import { browser } from '$app/environment';
+//import { browser } from '$app/environment';
 
 
-let playersInfo = null;
 
-if(browser) {
-    playersInfo = JSON.parse(localStorage.getItem("playersInfo"));
-}
-
-if(!playersInfo) {
-
-    const res  = await fetch(`https://api.sleeper.app/v1/players/nfl`, {compress: true}); 
-    const data = await res.json();
-    if(browser) {
-        localStorage.setItem("playersInfo", JSON.stringify(data))
-    }
-
-}
 
 //console.log(JSON.parse(localStorage.getItem("playersData")));
