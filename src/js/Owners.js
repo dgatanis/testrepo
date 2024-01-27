f391923c14e473e30ff7a6459ccf78d3de20ea50
dgatanis <122374677+dgatanis@users.noreply.github.com>
2024-01-27 11:35:07 -0700

    asdf

--- src/js/Owners.js
+++ src/js/Owners.js
@@ -89,47 +89,31 @@ async function getOwnerAvatarForLeague(leagueId,userid=-1) {
 }
 
 
-async function getTeamNamesForPowerRank(leagueId,userid=-1) { 
+async function getTeamNamesForPowerRank(leagueId) { 
     const usersResponse = await fetch(`https://api.sleeper.app/v1/league/${leagueId}/users`);
     const usersData = await usersResponse.json();
-    if(userid==-1)
-    {
-        const users = usersData.map((user) => user);
-        var powerRank = 1;
-        for (let user of users)
-        {
-            var powerRankingElementId = "PowerRanking_"+powerRank;
-            var rosterButtonId = "GetRosterButton_"+powerRank;
-            var powerRanking = document.getElementById(powerRankingElementId);
-            var rosterButton = document.getElementById(rosterButtonId);
 
-            if(user.metadata.team_name != undefined)
-            {
-                powerRanking.append(user.metadata.team_name);
-                rosterButton.setAttribute("onclick", "OpenTeamRosterModal(" + user.user_id + ", '" + user.metadata.team_name + "')");
-            }
-            else
-            {
-                powerRanking.append(user.display_name);
-                rosterButton.setAttribute("onclick", "OpenTeamRosterModal(" + user.user_id + ", '" + user.display_name + "')");
-            }
-            rosterButton.setAttribute('title', 'Look at their wack ass lineup.');
-            powerRank++;
-        }
-    }
-    else
+    const users = usersData.map((user) => user);
+
+    for (let user of users)
     {
-        const users = usersData.map((user) => user);
-        
-        for (let user of users)
+        var powerRankingElementId = "PowerRankTeamList";
+        var powerRanking = document.getElementById(powerRankingElementId);
+
+        if(user.metadata.team_name != undefined)
         {
-            if(user.user_id==userid)
-            {
-                var powerRanking = document.getElementById("PowerRanking_1");
-                powerRanking.innerHTML=user.metadata.team_name
-            }
+            var teamList = document.createElement("option");
+            teamList.setAttribute("value", user.user_id);
+            teamList.innerText=user.metadata.team_name;
+            powerRanking.prepend(teamList);
         }
-        return "displayNames";
+        else
+        {
+            var teamList = document.createElement("option");
+            teamList.setAttribute("value", user.user_id);
+            teamList.innerText=user.display_name;
+            powerRanking.prepend(teamList);
+        }
     }
     
 }
