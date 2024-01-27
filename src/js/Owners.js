70312d194c5c4c22186ab4d07c5280f1b78a747e
dgatanis <122374677+dgatanis@users.noreply.github.com>
2024-01-27 10:42:29 -0700

    asdf

--- src/js/Owners.js
+++ src/js/Owners.js
@@ -86,6 +86,50 @@ async function getOwnerAvatarForLeague(leagueId,userid=-1) {
 
         }
     }
+}
 
 
+async function getTeamNamesForPowerRank(leagueId,userid=-1) { 
+    const usersResponse = await fetch(`https://api.sleeper.app/v1/league/${leagueId}/users`);
+    const usersData = await usersResponse.json();
+    if(userid==-1)
+    {
+        const users = usersData.map((user) => user);
+        var powerRank = 1;
+        for (let user of users)
+        {
+            var powerRankingElementId = "PowerRanking_"+powerRank;
+            var rosterButtonId = "GetRosterButton_"+powerRank;
+            var powerRanking = document.getElementById(powerRankingElementId);
+            var rosterButton = document.getElementById(rosterButtonId);
+
+            if(user.metadata.team_name != undefined)
+            {
+                powerRanking.append(user.metadata.team_name);
+                rosterButton.setAttribute("onclick", "OpenTeamRosterModal(" + user.user_id + ", '" + user.metadata.team_name + "')");
+            }
+            else
+            {
+                powerRanking.append(user.display_name);
+                rosterButton.setAttribute("onclick", "OpenTeamRosterModal(" + user.user_id + ", '" + user.display_name + "')");
+            }
+            rosterButton.setAttribute('title', 'Look at their wack ass lineup.');
+            powerRank++;
+        }
+    }
+    else
+    {
+        const users = usersData.map((user) => user);
+        
+        for (let user of users)
+        {
+            if(user.user_id==userid)
+            {
+                var powerRanking = document.getElementById("PowerRanking_1");
+                powerRanking.innerHTML=user.metadata.team_name
+            }
+        }
+        return "displayNames";
+    }
+    
 }
