6434d51a08990a1e6238ccfa2690771be9be9955
dgatanis <122374677+dgatanis@users.noreply.github.com>
2024-01-27 12:17:33 -0700

    asdf

--- src/js/PowerRankings/PowerRankings.js
+++ src/js/PowerRankings/PowerRankings.js
@@ -1,4 +1,4 @@
-async function getTeamNamesForPowerRank(leagueId) { 
+async function loadPowerRankModal(leagueId) { 
     const usersResponse = await fetch(`https://api.sleeper.app/v1/league/${leagueId}/users`);
     const usersData = await usersResponse.json();
 
@@ -6,26 +6,63 @@ async function getTeamNamesForPowerRank(leagueId) {
 
     for (let i=0; i<users.length; i++)
     {
-        var powerRankingElementId = "PowerRankTeamList";
-        var powerRanking = document.getElementById(powerRankingElementId);
-
-        if(users[i].metadata.team_name != undefined)
-        {
-            var teamList = document.createElement("option");
-            teamList.setAttribute("id", users[i].user_id);
-            teamList.setAttribute("value", (i+1))
-            teamList.innerText=users[i].metadata.team_name;
-            powerRanking.prepend(teamList);
-        }
-        else
-        {
-            var teamList = document.createElement("option");
-            teamList.setAttribute("value", users[i].user_id);
-            teamList.innerText=users[i].display_name;
-            powerRanking.prepend(teamList);
-        }
+        var updateForm = document.getElementById("UpdatePowerRankList");
+        var newRow = createPowerRankList(i, users[i].user_id);
+        updateForm.append(newRow);
+        // if(users[i].metadata.team_name != undefined)
+        // {
+        //     var teamList = document.createElement("option");
+        //     teamList.setAttribute("id", users[i].user_id);
+        //     teamList.setAttribute("value", (i+1))
+        //     teamList.innerText=users[i].metadata.team_name;
+        //     powerRanking.prepend(teamList);
+        // }
+        // else
+        // {
+        //     var teamList = document.createElement("option");
+        //     teamList.setAttribute("value", users[i].user_id);
+        //     teamList.setAttribute("value", (i+1))
+        //     teamList.innerText=users[i].display_name;
+        //     powerRanking.prepend(teamList);
+        // }
     }
     
+}
+
+function createPowerRankList (id, userid) {
+    var powerRankRow = document.createElement("div");
+    powerRankRow.setAttribute("id", "PowerRankRow_" + id);
+    powerRankRow.setAttribute("class", "row");
+
+    var rankTeamGroup = document.createElement("div");
+    rankTeamGroup.setAttribute("class", "col");
+
+    var ranking = document.createElement("input");
+    ranking.setAttribute("type", "text");
+    ranking.setAttribute("class", "form-control custom-powerrank")
+    ranking.setAttribute("readonly", true);
+
+    var selectList = document.createElement("select");
+    selectList.setAttribute("id", "PowerRankList");
+    selectList.setAttribute("class", "form-select custom-powerrank-team");
+    selectList.setAttribute("aria-label", "PowerRankTeamList");
+
+    powerRankRow.prepend(rankTeamGroup);
+    rankTeamGroup.prepend(ranking);
+    rankTeamGroup.prepend(selectList);
+
+    var comments = document.createAttribute("div");
+    comments.setAttribute("class", "col");
+
+    var commentText = document.createElement("textarea");
+    commentText.setAttribute("class", "custom-powerrank-comments");
+    commentText.setAttribute("type", "text");
+    commentText.setAttribute("rows", 3);
+    commentText.setAttribute("placeholder", "Comments");
+
+    powerRankRow.append(comments);
+
+    return powerRankRow;
 }
 
 async function getTeamNamesForLeague(leagueId,userid=-1) { 
@@ -71,4 +108,42 @@ async function getTeamNamesForLeague(leagueId,userid=-1) {
         return "displayNames";
     }
     
+}
+
+
+async function OpenTeamRosterModal(userid,teamname) {
+    const rosterResponse = await fetch(`https://api.sleeper.app/v1/league/1046222222567784448/rosters`); 
+    const rosterData = await rosterResponse.json(); 
+    
+    var modalRosterTeamName = document.querySelector('#ModalRosterTeamName');
+    var rosterTable = document.querySelector('#RosterTable');
+    var tablebody = rosterTable.childNodes[3];
+    
+    //show table and set team name
+    modalRosterTeamName.innerText = teamname;
+  
+    //Remove players in list
+    while(tablebody.firstChild) {
+      tablebody.removeChild(tablebody.firstChild);
+    }
+  
+    //Create table rows for players
+    const teams = rosterData.map((roster) => roster);
+    for(let roster of teams) {
+      if(roster.owner_id==userid)
+      {
+        for(let players of roster.players)
+          {
+            var tr = document.createElement("tr");
+            var th = document.createElement("th");
+            th.innerText="Position";
+            th.setAttribute('scope', 'row');
+            tr.appendChild(th);
+            var td = document.createElement("td");
+            td.innerText=players;
+            tr.appendChild(td);
+            tablebody.append(tr);
+          }
+      }
+    }
 }
