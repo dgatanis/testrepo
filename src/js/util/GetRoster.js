94fbd413a005ae04ef6f6eb9c65e5813330403cc
dgatanis <122374677+dgatanis@users.noreply.github.com>
2024-01-28 09:35:36 -0700

    asdf

--- src/js/util/GetRoster.js
+++ src/js/util/GetRoster.js
@@ -4,5 +4,5 @@ async function getRostersForLeague(leagueID){
     const rosterResponse = await fetch(`https://api.sleeper.app/v1/league/${leagueID}/rosters`); 
     const rosterData = await rosterResponse.json(); 
 
-    console.log(rosterData);
+    localStorage.setItem("RosterData", rosterData);
   }
