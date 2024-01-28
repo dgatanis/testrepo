33d2e5ee00efd5953643942ce6f002b248a00ec1
dgatanis <122374677+dgatanis@users.noreply.github.com>
2024-01-28 10:42:47 -0700

    asdf

--- src/js/History/History.js
+++ src/js/History/History.js
@@ -1,8 +1,23 @@
 //Bankroll function
-function getBankRoll () {
+function getBankRoll (userid) {
+
     if(localStorage.getItem("RosterData"))
     {
+        let rosterData = localStorage.getItem("RosterData");
+        let rosters = JSON.parse(rosterData);
         
+        let wins = null;
+        let losses = null;
+        let test = null;
+
+        for(let roster of rosters)
+        {
+            if(roster.owner_id == userid)
+            {
+                test = roster.waiver_position;
+                console.log(test);
+            }
+        }
     }
 
 }
