1beaabc67958ef83415824270bd2854ba7347979
dgatanis <122374677+dgatanis@users.noreply.github.com>
2024-01-27 19:49:39 -0700

    asdf

--- src/js/PowerRankings/PowerRankings.js
+++ src/js/PowerRankings/PowerRankings.js
@@ -210,8 +210,8 @@ function validateComments() {
     {
         if(comment.value == '')
         {
-            //alert("Please add comments to all rankings");
-            //return
+            comment.setCustomValidity("Please add comments to all rankings");
+            return
         }
     }
 } 
