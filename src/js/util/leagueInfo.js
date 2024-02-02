e4ead25ab9c67f362cc93cefa4e75275472a5aab
dgatanis <122374677+dgatanis@users.noreply.github.com>
2024-02-02 16:06:02 -0700

    asdf

--- src/js/util/leagueInfo.js
+++ src/js/util/leagueInfo.js
@@ -1,4 +1,4 @@
-export const leagueID = "1046222222567784448";
+export const leagueID = currentLeagueId();
 export const leagueName = "Crush Cities "; // your league name
 export const dues = 200; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
@@ -6,7 +6,7 @@ export const dynasty = true; // true for dynasty leagues, false for redraft and 
 export const firstYear = 2024;
 
 
-export default async function currentLeagueId() {
+async function currentLeagueId() {
     const thisYear = new Date().getFullYear;
     const myUserId = '467550885086490624';
     const userLeagues = await fetch(`https://api.sleeper.app/v1/user/${myUserId}/leagues/nfl/${thisYear}`);
