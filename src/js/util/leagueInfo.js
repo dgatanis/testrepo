22833c150b988677dc2e93d8df54ea8c19a5eed2
dgatanis <122374677+dgatanis@users.noreply.github.com>
2024-02-02 16:02:55 -0700

    asdf

--- src/js/util/leagueInfo.js
+++ src/js/util/leagueInfo.js
@@ -1,4 +1,4 @@
-export const leagueId = "1046222222567784448";
+export const leagueID = "1046222222567784448";
 export const leagueName = "Crush Cities "; // your league name
 export const dues = 200; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
