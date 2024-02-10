6d4aecd9b68d1667e78afe49b2df83666ba903b7
dgatanis <122374677+dgatanis@users.noreply.github.com>
2024-02-10 16:26:17 -0700

    asdf

--- src/js/CurrentSeason/currentSeason.js
+++ src/js/CurrentSeason/currentSeason.js
@@ -586,7 +586,7 @@ function createMatchupListElement(weekNumber) {
 
 function createMatchupWeekHighScorerImg(){
     var img = document.createElement("img");
-    img.setAttribute('src', '../src/static/images/crown-2.png');
+    img.setAttribute('src', '../src/static/images/crown-icon.png');
     img.setAttribute('class', "custom-highscorer");
     
     return img;
