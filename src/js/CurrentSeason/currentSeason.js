8ad1738cfdb544fa0aced6768d735872eaea1a18
dgatanis <122374677+dgatanis@users.noreply.github.com>
2024-02-10 13:37:54 -0700

    asdf

--- src/js/CurrentSeason/currentSeason.js
+++ src/js/CurrentSeason/currentSeason.js
@@ -4,13 +4,14 @@ const userDataStorage = localStorage.getItem("UserData");
 const userData = JSON.parse(userDataStorage);
 const playerDataStorage = localStorage.getItem("PlayerData")
 const playerData = JSON.parse(playerDataStorage); 
-
+var varLeagueId;
 async function loadConstants() {
 
     const leagueInfo = await import('../util/leagueInfo.js');
     var leagueInfoLeagueId = leagueInfo.default();
 
     leagueInfoLeagueId.then((currentLeagueId) => {
+        varLeagueId = currentLeagueId;
         loadSeasonRankings(currentLeagueId);
         loadMatchupsList();
     }).catch((error) => {
@@ -59,8 +60,8 @@ function loadSeasonRankings(leagueId) {
 async function loadMatchups(weekNumber) {
 
     //Need to change matchups to our league when go live
-    const matchup = await fetch(`https://api.sleeper.app/v1/league/1003692635549462528/matchups/${weekNumber}`);
-    //const matchup = await fetch(`https://api.sleeper.app/v1/league/${leagueId}/matchups/${weekNumber}`);
+    //const matchup = await fetch(`https://api.sleeper.app/v1/league/1003692635549462528/matchups/${weekNumber}`);
+    const matchup = await fetch(`https://api.sleeper.app/v1/league/${varLeagueId}/matchups/${weekNumber}`);
     const matchupData = await matchup.json(); 
     const matchups = matchupData.map((team) => team);
     const highScoreTeam = getRosterHighScorerWeek(matchups);
