ef3fd41870d06ae6a155feae5e136eee52f29172
dgatanis <122374677+dgatanis@users.noreply.github.com>
2024-02-09 18:05:19 -0700

    asdf

--- src/js/CurrentSeason/currentSeason.js
+++ src/js/CurrentSeason/currentSeason.js
@@ -306,7 +306,7 @@ function rosterStats(rosterid) {
     {
         var playerPositionCount = calcPlayerPositions(roster.players);
         var playerAge = calcPlayerAge(roster.players);
-        var playerAge = "QB: " + playerPositionCount[0].QB + " RB: " + playerPositionCount[0].RB + " TE: " + playerPositionCount[0].TE + " WR: " + playerPositionCount[0].WR + " K: " + playerPositionCount[0].K; + " Average age of roster: " + playerAge[0].AvgAge;
+        var playerAge = "QB: " + playerPositionCount[0].QB + " RB: " + playerPositionCount[0].RB + " TE: " + playerPositionCount[0].TE + " WR: " + playerPositionCount[0].WR + " K: " + playerPositionCount[0].K + " Average age of roster: " + playerAge[0].AvgAge;
         return playerAge;
     }
 }
