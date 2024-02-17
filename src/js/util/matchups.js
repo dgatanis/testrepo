ea4b91da0ab61de99fdedbe02a03fd73d3cd6f36
dgatanis <122374677+dgatanis@users.noreply.github.com>
2024-02-17 11:36:10 -0700

    asdf

--- src/js/util/matchups.js
+++ src/js/util/matchups.js
@@ -6,7 +6,7 @@ const leagueData = JSON.parse(leagueDataStorage);
 const currentWeek = leagueInfo.getCurrentWeek();
 
 currentWeek.then((thisWeek) => {
-    setBrowserData(leagueData.league_id,thisWeek);
+    setBrowserData(leagueData.league_id,5);
 }).catch((error) => {
     console.error(`Error: ${error.message}`);
 });
@@ -23,10 +23,28 @@ function setMatchupData(leagueID,currentWeek){
 }
 
 async function getMatchupData(leagueID, currentWeek) {
-    //const matchup = await fetch(`https://api.sleeper.app/v1/league/1003692635549462528/matchups/2`);
-    const matchup = await fetch(`https://api.sleeper.app/v1/league/${leagueID}/matchups/${currentWeek}`);
-    const matchupData = await matchup.json(); 
-    console.log(leagueID);
-    console.log("week: " + currentWeek);
-    sessionStorage.setItem("MatchupData", JSON.stringify(matchupData));
+
+    totalWeeksPlayed = parseInt(currentWeek);
+    leagueID = '1003692635549462528'; //TESTING LEAGUE
+    let matchups = {};
+
+    for(let i = 1; i<=totalWeeksPlayed; i++)
+    {
+        //const matchup = await fetch(`https://api.sleeper.app/v1/league/1003692635549462528/matchups/2`);
+        const matchup = await fetch(`https://api.sleeper.app/v1/league/${leagueID}/matchups/${i}`);
+        const matchupData = await matchup.json(); 
+        //console.log(leagueID);
+        //console.log("week: " + currentWeek);
+
+        if(matchupData)
+        {
+            matchups.push({
+                ...matchupData[0]
+            });
+        }
+        
+    }
+
+    sessionStorage.setItem("MatchupData", JSON.stringify(matchups));
+
 }
