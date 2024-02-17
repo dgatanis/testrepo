95f28488ca55327d73b4d888b9df4ee80d8e7588
dgatanis <122374677+dgatanis@users.noreply.github.com>
2024-02-17 11:10:17 -0700

    asdf

--- src/js/util/matchups.js
+++ src/js/util/matchups.js
@@ -22,7 +22,7 @@ function setMatchupData(leagueID,currentWeek){
 
 async function getMatchupData(leagueID, currentWeek) {
     //const matchup = await fetch(`https://api.sleeper.app/v1/league/1003692635549462528/matchups/2`);
-    const matchup = await fetch(`https://api.sleeper.app/v1/league/${leagueId}/matchups/${weekNumber}`);
+    const matchup = await fetch(`https://api.sleeper.app/v1/league/${leagueID}/matchups/${weekNumber}`);
     const matchupData = await matchup.json(); 
     console.log(leagueID);
     console.log("week: " + currentWeek);
