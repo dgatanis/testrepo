ebadfea4c9d5e998da618cda7e3e805e44fe18b5
dgatanis <122374677+dgatanis@users.noreply.github.com>
2024-02-19 15:35:43 -0700

    asdf

--- src/js/CurrentSeason/currentSeason.js
+++ src/js/CurrentSeason/currentSeason.js
@@ -271,7 +271,7 @@ function loadBankroll(week,dues,weeklyWinnerPayout) {
         let row = document.getElementById("bankrollTeam_" + i);
         let rowTeam = row.getElementsByTagName('th');
         let rowBankRoll = row.getElementsByTagName('td');
-        let rowTeamName = rowTeam[0].getElementsByClassName('custom-teamname-small');
+        let rowTeamName = rowTeam[0].getElementsByClassName('custom-teamname-normal');
 
         var ownerAvatar = createOwnerAvatarImage(rosterBankrolls[i].user_id);
         var teamName = getTeamName(rosterBankrolls[i].user_id);
