a11504fb6f937d6be1419ca79d54363bf36dd31d
dgatanis <122374677+dgatanis@users.noreply.github.com>
2024-03-02 18:48:37 -0700

    asdf

--- src/js/CurrentSeason/currentSeason.js
+++ src/js/CurrentSeason/currentSeason.js
@@ -905,17 +905,15 @@ function highestScorerByPosition(rosterid) {
         }
         return 0;
     });
-    console.log(teamQB[0]);
-    console.log(teamRB[0]);
-    console.log(teamWR[0]);
-    console.log(teamTE[0]);
+
     let highScoringPlayers = {
         ...teamQB[0],
         ...teamRB[0],
         ...teamWR[0],
         ...teamTE[0]
     }
-    console.log(highScoringPlayers);
+    
+    return highScoringPlayers;
 
 }
 
@@ -1056,14 +1054,15 @@ function getRosterStats(rosterid) {
 
     const rosters = rosterData.map((x) => x);
     let roster = rosters.find(x => x.roster_id === parseInt(rosterid));
-    console.log(highestScorerByPosition(rosterid));
+    
 
     if(roster)
     {
+        var test = highestScorerByPosition(rosterid);
         var playerPositionCount = calcPlayerPositions(roster.players);
         var playerAge = calcPlayerAge(roster.players);
         var teamRecord = getTeamRecord(rosterid);
-
+        console.log(test);
         let rosterStats = {
             ...playerPositionCount[0],
             ...playerAge[0],
