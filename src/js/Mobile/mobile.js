c1a04e94a10d0bde797b389685d962a59275d60c
dgatanis <122374677+dgatanis@users.noreply.github.com>
2024-03-03 09:39:00 -0700

    asdf

---
+++ src/js/Mobile/mobile.js
@@ -0,0 +1,9 @@
+//Custom mobile navigation 
+function toggleMobileMenu() {
+    var x = document.getElementById("mobileLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
