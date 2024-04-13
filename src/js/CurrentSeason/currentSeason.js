2889a1f081dd5536cac20b2d4985895b0e48e7ad
dgatanis <122374677+dgatanis@users.noreply.github.com>
2024-04-13 12:18:47 -0600

    asdf

--- src/js/CurrentSeason/currentSeason.js
+++ src/js/CurrentSeason/currentSeason.js
@@ -1636,7 +1636,7 @@ function createMatchupListElement(weekNumber) {
     var firstListItem = document.createElement("li");
     firstListItem.setAttribute("class", "h4 list-group-item custom-matchup-list-item shadow p-3 mb-5 bg-body rounded");
     firstListItem.innerText="Matchups and High Scorer";
-    firstListItem.setAttribute('style', '')
+    firstListItem.setAttribute('style', '');
 
     var noMatchups = document.createElement("div");
     noMatchups.setAttribute("class", "custom-block-display custom-nomatchup");
