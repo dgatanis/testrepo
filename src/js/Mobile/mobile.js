57ae04edc21f37e8aae8c980e2db83fb3e9855be
dgatanis <122374677+dgatanis@users.noreply.github.com>
2024-04-27 11:34:18 -0600

    adsf

--- src/js/Mobile/mobile.js
+++ src/js/Mobile/mobile.js
@@ -3,7 +3,8 @@ function toggleMobileMenu() {
     var x = document.getElementById("mobileLinks");
     if (x.style.display === "block") {
         x.style.display = "none";
+        x.setAttribute('style', 'display:none;background: transparent;');
     } else {
-        x.style.display = "block";
+        x.setAttribute('style', 'display:block;animation: revealLetter 1s forwards; background: rgb(97, 97, 97);');
     }
 }
