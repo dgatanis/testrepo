e0cdd7c862983af869e728060a33b969fbac78c0
dgatanis <122374677+dgatanis@users.noreply.github.com>
2024-04-28 18:58:00 -0600

    asdf

--- src/js/Mobile/mobile.js
+++ src/js/Mobile/mobile.js
@@ -2,8 +2,8 @@
 function toggleMobileMenu() {
     var x = document.getElementById("mobileLinks");
     if (x.style.visibility === "visible") {
-        x.setAttribute('style', 'visibility: hidden;');
+        x.setAttribute('class', 'custom-mobile-hide');
     } else {
-        x.setAttribute('style', 'visibility: visible;background: rgb(97, 97, 97);display: block;');
+        x.setAttribute('class', 'custom-mobile-show');
     }
 }
