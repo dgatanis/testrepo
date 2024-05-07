3d0632dd09dfdf2c499ec57478bd34eed762ef63
dgatanis <122374677+dgatanis@users.noreply.github.com>
2024-05-07 17:55:27 -0600

    asdf

--- src/js/Home/home.js
+++ src/js/Home/home.js
@@ -84,32 +84,41 @@ function loadContents() {
 
 function loadTeams() {
 
-    var teams = document.querySelectorAll('[id*=team]');
+    try{
+        var teams = document.querySelectorAll('[id*=team]');
 
-    for(let i = 0; i < teams.length; i++)
-    {
-        var champRosterId = playoffData[playoffData.length-1].w;
-        let rosterid = i + 1;
-        let roster = rosterData.find(x => x.roster_id === rosterid);
-        let teamName = getTeamName(roster.owner_id);
-        let playerImg = createOwnerAvatarImage(roster.owner_id);
-
-        teams[i].children[0].children[0].innerText = teamName;
-        teams[i].children[0].prepend(playerImg);
-        teams[i].children[0].setAttribute('onclick', 'openRostersPage(' + roster.roster_id + ')');
-
-        if(champRosterId == roster.roster_id)
+        for(let i = 0; i < teams.length; i++)
         {
-            var throneImg = document.createElement('img');
-            throneImg.setAttribute('src','src/static/images/throne.png');
-            throneImg.setAttribute('class', 'custom-throne-icon-medium');
-            throneImg.setAttribute('title', 'The Throne');
-
-
-            teams[i].children[0].append(throneImg);
+            var champRosterId = playoffData[playoffData.length-1].w;
+            let rosterid = i + 1;
+            let roster = rosterData.find(x => x.roster_id === rosterid);
+            let teamName = getTeamName(roster.owner_id);
+            let playerImg = createOwnerAvatarImage(roster.owner_id);
+    
+            teams[i].children[0].children[0].innerText = teamName;
+            teams[i].children[0].prepend(playerImg);
+            teams[i].children[0].setAttribute('onclick', 'openRostersPage(' + roster.roster_id + ')');
+    
+            if(champRosterId == roster.roster_id)
+            {
+                var throneImg = document.createElement('img');
+                throneImg.setAttribute('src','src/static/images/throne.png');
+                throneImg.setAttribute('class', 'custom-throne-icon-medium');
+                throneImg.setAttribute('title', 'The Throne');
+    
+    
+                teams[i].children[0].append(throneImg);
+            }
+            
         }
-        
     }
+
+    catch (error)
+    {
+        console.error(error.message);
+    }
+
+
 
 }
 
