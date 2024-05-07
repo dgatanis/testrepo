15275360f985ff830ab070904fd7b5d7157dbb9c
dgatanis <122374677+dgatanis@users.noreply.github.com>
2024-05-06 18:29:05 -0600

    asdf

--- src/js/Home/home.js
+++ src/js/Home/home.js
@@ -84,6 +84,7 @@ function loadTeams() {
 
     for(let i = 0; i < teams.length; i++)
     {
+        var champRosterId = playoffData[playoffData.length-1].w;
         let rosterid = i + 1;
         let roster = rosterData.find(x => x.roster_id === rosterid);
         let teamName = getTeamName(roster.owner_id);
@@ -92,6 +93,15 @@ function loadTeams() {
         teams[i].children[0].children[0].innerText = teamName;
         teams[i].children[0].prepend(playerImg);
         teams[i].children[0].setAttribute('onclick', 'openRostersPage(' + roster.roster_id + ')');
+
+        if(champRosterId == roster.roster_id)
+        {
+            var throneImg = document.createElement('img');
+            throneImg.setAttribute('src','src/static/images/throne.png');
+            throneImg.setAttribute('class', 'custom-throne-icon-medium');
+            
+            teams[i].children[0].prepend(throneImg);
+        }
         
     }
 
