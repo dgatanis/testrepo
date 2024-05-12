b39f7b17c9197bdf89079df6f01dfc9129478c5b
dgatanis <122374677+dgatanis@users.noreply.github.com>
2024-05-12 08:36:33 -0600

    asdf

--- src/js/Rosters/rosters.js
+++ src/js/Rosters/rosters.js
@@ -953,24 +953,24 @@ function createNFLTeamImage(team) {
     return teamImage;
 }
 
-// function createOwnerAvatarImage(userId) { 
+function createOwnerAvatarImage(userId) { 
 
-//     let user = userData.find(x => x.user_id === userId);
-//     const avatarURL = user.metadata.avatar;
+    let user = userData.find(x => x.user_id === userId);
+    const avatarURL = user.metadata.avatar;
     
-//     if(avatarURL)
-//     {
-//         var img = document.createElement("img");
-//         img.setAttribute('src', avatarURL);
-//         img.setAttribute('class', "custom-medium-avatar");
-//         img.setAttribute('data-userid', user.user_id);
-//     }
-//     else
-//     {
-//         var img = document.createElement("img");
-//         img.setAttribute('src', '../src/static/images/trashcan.png');
-//         img.setAttribute('class', "custom-medium-avatar");
-//         img.setAttribute('data-userid', user.user_id);
-//     }
-//     return img;
-// }
+    if(avatarURL)
+    {
+        var img = document.createElement("img");
+        img.setAttribute('src', avatarURL);
+        img.setAttribute('class', "custom-medium-avatar");
+        img.setAttribute('data-userid', user.user_id);
+    }
+    else
+    {
+        var img = document.createElement("img");
+        img.setAttribute('src', '../src/static/images/trashcan.png');
+        img.setAttribute('class', "custom-medium-avatar");
+        img.setAttribute('data-userid', user.user_id);
+    }
+    return img;
+}
--- src/js/Rosters/testing.js
+++ src/js/Rosters/testing.js
@@ -2,6 +2,6 @@ import { rosterDatas, leagueDatas, userDatas } from '../util/helper.js';
 
 //var x = createOwnerAvatarImage('861092508472578048');
 var userData = userDatas;
-
+console.log(userData);
 let user = userData.find(x => x.user_id === '861092508472578048');
 console.log(user.display_name);
