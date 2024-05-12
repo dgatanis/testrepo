524c3006360c238c75a20486d5c191886347486b
dgatanis <122374677+dgatanis@users.noreply.github.com>
2024-05-12 12:34:06 -0600

    asdf

---
+++ src/js/Home/functions.js
@@ -0,0 +1,4 @@
+function openRostersPage(rosterid) {
+    window.location.assign('/testrepo/web/Rosters.html?callFunction=openRoster&rosterId='+rosterid);
+    return;
+}
--- src/js/Home/home.js
+++ src/js/Home/home.js
@@ -1,89 +1,20 @@
-const rosterDataStorage = localStorage.getItem("RosterData");
-var rosterData = JSON.parse(rosterDataStorage); 
-const userDataStorage = localStorage.getItem("UserData");
-var userData = JSON.parse(userDataStorage);
-const matchupWeekStorage = sessionStorage.getItem("MatchupData");
-var matchupData = JSON.parse(matchupWeekStorage); 
-const playerDataStorage = localStorage.getItem("PlayerData");
-var playerData = JSON.parse(playerDataStorage); 
-const playoffDataStorage = localStorage.getItem("PlayoffData");
-var playoffData = JSON.parse(playoffDataStorage); 
+import { 
+    rosters, 
+    users, 
+    players, 
+    createOwnerAvatarImage, 
+    getTeamName,
+    playoffs
+    } from '../util/helper.js';
 
-async function checkBrowserData() {
+let userData = users;
+let rosterData = rosters;
+let playerData = players;
+let playoffData = playoffs;
 
-    if(!rosterData || !playoffData) //check for matchup data too because its stored in sessionstorage
-    {
-        try{
-            initBrowserData();
-        }
-        catch (error){
-            console.error(`Error: ${error.message}`);
-        }
-    }
-    else
-    {
-        loadContents();
-    }
-}
+loadContents();
 
-function waitForLocalStorageItem(key) {
-    return new Promise((resolve) => {
-        const checkLocalStorage = () => {
-            const item = localStorage.getItem(key);
-            if (item !== null) {
-                resolve(item);
-            } else {
-                setTimeout(checkLocalStorage, 100); // Check again in 100 milliseconds
-            }
-        };
-        checkLocalStorage();
-    });
-}
-
-function waitForSessionStorageItem(key) {
-    return new Promise((resolve) => {
-        const checkSessionStorage = () => {
-            const item = sessionStorage.getItem(key);
-            if (item !== null) {
-                resolve(item);
-            } else {
-                setTimeout(checkSessionStorage, 100); // Check again in 100 milliseconds
-            }
-        };
-        checkSessionStorage();
-    });
-}
-
-async function initBrowserData() {
-    try {
-        const localRosterData = await waitForLocalStorageItem("RosterData");
-        const localLeagueData = await waitForLocalStorageItem("LeagueData");
-        const localPlayerData = await waitForLocalStorageItem("PlayerData");
-        const localUserData = await waitForLocalStorageItem("UserData");
-        const localPlayoffData = await waitForLocalStorageItem("PlayoffData");
-        const localMatchupData = await waitForSessionStorageItem("MatchupData");
-        
-
-        rosterData = JSON.parse(localRosterData);
-        userData = JSON.parse(localUserData);
-        playerData = JSON.parse(localPlayerData);
-        leagueData = JSON.parse(localLeagueData);
-        matchupData = JSON.parse(localMatchupData);
-        playoffData = JSON.parse(localPlayoffData);
-
-        loadContents();
-
-    } catch (error) {
-        console.error('Error loading or executing script:', error);
-    }
-}
-
-//This loads the page contents dynamically
 function loadContents() {
-    loadTeams();
-}
-
-function loadTeams() {
 
     try{
         var teams = document.querySelectorAll('[id*=team]');
@@ -121,48 +52,4 @@ function loadTeams() {
 
 
 
-}
-
-function getTeamName(userid) {
-
-    let user = userData.find(x => x.user_id === userid.toString());
-    let userName = "";
-
-    if(user.metadata.team_name != undefined)
-    {
-        userName = user.metadata.team_name;
-    }
-    else
-    {
-        userName = user.display_name;
-    }
-
-    return userName.toString();
-}
-
-function createOwnerAvatarImage(userId) { 
-
-    let user = userData.find(x => x.user_id === userId);
-    const avatarURL = user.metadata.avatar;
-    
-    if(avatarURL)
-    {
-        var img = document.createElement("img");
-        img.setAttribute('src', avatarURL);
-        img.setAttribute('class', "custom-medium-avatar");
-        img.setAttribute('data-userid', user.user_id);
-    }
-    else
-    {
-        var img = document.createElement("img");
-        img.setAttribute('src', 'src/static/images/trashcan.png');
-        img.setAttribute('class', "custom-medium-avatar");
-        img.setAttribute('data-userid', user.user_id);
-    }
-    return img;
-}
-
-function openRostersPage(rosterid) {
-    window.location.assign('/testrepo/web/Rosters.html?callFunction=openRoster&rosterId='+rosterid);
-    return;
 }
