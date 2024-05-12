ff696b7b30eae4485120e799ba6c1aa11792b509
dgatanis <122374677+dgatanis@users.noreply.github.com>
2024-05-12 08:39:50 -0600

    asdf

--- src/js/Rosters/testing.js
+++ src/js/Rosters/testing.js
@@ -1,7 +1,12 @@
 import { rosterDatas, leagueDatas, userDatas } from '../util/helper.js';
 
+var userData;
+userDatas.then((value) => {
+    userData = value;
+  }).catch((error) => {
+    console.error('Error:', error);
+  });
 //var x = createOwnerAvatarImage('861092508472578048');
-var userData = userDatas;
 console.log(userData);
 let user = userData.find(x => x.user_id === '861092508472578048');
 console.log(user.display_name);
