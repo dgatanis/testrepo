39078ffbfa463ac9d45c516909fcd78edca30404
dgatanis <122374677+dgatanis@users.noreply.github.com>
2024-05-12 07:27:13 -0600

    asdf

--- src/js/Rosters/rosters.js
+++ src/js/Rosters/rosters.js
@@ -7,6 +7,7 @@ var matchupData = JSON.parse(matchupWeekStorage);
 const playerDataStorage = localStorage.getItem("PlayerData");
 var playerData = JSON.parse(playerDataStorage); 
 
+import { createOwnerAvatarImage } from '../src/js/util/helper.js'
 
 async function checkBrowserData() {
 
@@ -953,24 +954,24 @@ function createNFLTeamImage(team) {
     return teamImage;
 }
 
-function createOwnerAvatarImage(userId) { 
+// function createOwnerAvatarImage(userId) { 
 
-    let user = userData.find(x => x.user_id === userId);
-    const avatarURL = user.metadata.avatar;
+//     let user = userData.find(x => x.user_id === userId);
+//     const avatarURL = user.metadata.avatar;
     
-    if(avatarURL)
-    {
-        var img = document.createElement("img");
-        img.setAttribute('src', avatarURL);
-        img.setAttribute('class', "custom-medium-avatar");
-        img.setAttribute('data-userid', user.user_id);
-    }
-    else
-    {
-        var img = document.createElement("img");
-        img.setAttribute('src', '../src/static/images/trashcan.png');
-        img.setAttribute('class', "custom-medium-avatar");
-        img.setAttribute('data-userid', user.user_id);
-    }
-    return img;
-}
+//     if(avatarURL)
+//     {
+//         var img = document.createElement("img");
+//         img.setAttribute('src', avatarURL);
+//         img.setAttribute('class', "custom-medium-avatar");
+//         img.setAttribute('data-userid', user.user_id);
+//     }
+//     else
+//     {
+//         var img = document.createElement("img");
+//         img.setAttribute('src', '../src/static/images/trashcan.png');
+//         img.setAttribute('class', "custom-medium-avatar");
+//         img.setAttribute('data-userid', user.user_id);
+//     }
+//     return img;
+// }
---
+++ src/js/util/helper.js
@@ -0,0 +1,74 @@
+const rosterDataStorage = localStorage.getItem("RosterData");
+var rosterData = JSON.parse(rosterDataStorage); 
+const userDataStorage = localStorage.getItem("UserData");
+var userData = JSON.parse(userDataStorage);
+const matchupWeekStorage = sessionStorage.getItem("MatchupData");
+var matchupData = JSON.parse(matchupWeekStorage); 
+const playerDataStorage = localStorage.getItem("PlayerData");
+var playerData = JSON.parse(playerDataStorage); 
+
+
+if(!rosterData|| !userData || !matchupData || !playerData)
+{
+    const localRosterData = await waitForLocalStorageItem("RosterData");
+    const localLeagueData = await waitForLocalStorageItem("LeagueData");
+    const localPlayerData = await waitForLocalStorageItem("PlayerData");
+    const localUserData = await waitForLocalStorageItem("UserData");
+    const localMatchupData = await waitForSessionStorageItem("MatchupData");
+
+    rosterData = JSON.parse(localRosterData);
+    userData = JSON.parse(localUserData);
+    playerData = JSON.parse(localPlayerData);
+    leagueData = JSON.parse(localLeagueData);
+    matchupData = JSON.parse(localMatchupData);
+}
+
+function waitForLocalStorageItem(key) {
+    return new Promise((resolve) => {
+        const checkLocalStorage = () => {
+            const item = localStorage.getItem(key);
+            if (item !== null) {
+                resolve(item);
+            } else {
+                setTimeout(checkLocalStorage, 100); // Check again in 100 milliseconds
+            }
+        };
+        checkLocalStorage();
+    });
+}
+
+function waitForSessionStorageItem(key) {
+    return new Promise((resolve) => {
+        const checkSessionStorage = () => {
+            const item = sessionStorage.getItem(key);
+            if (item !== null) {
+                resolve(item);
+            } else {
+                setTimeout(checkSessionStorage, 100); // Check again in 100 milliseconds
+            }
+        };
+        checkSessionStorage();
+    });
+}
+
+function createOwnerAvatarImage(userId) { 
+
+    let user = userData.find(x => x.user_id === userId);
+    const avatarURL = user.metadata.avatar;
+    
+    if(avatarURL)
+    {
+        var img = document.createElement("img");
+        img.setAttribute('src', avatarURL);
+        img.setAttribute('class', "custom-medium-avatar");
+        img.setAttribute('data-userid', user.user_id);
+    }
+    else
+    {
+        var img = document.createElement("img");
+        img.setAttribute('src', '../src/static/images/trashcan.png');
+        img.setAttribute('class', "custom-medium-avatar");
+        img.setAttribute('data-userid', user.user_id);
+    }
+    return img;
+}
