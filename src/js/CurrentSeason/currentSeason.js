c3db017a6bc3e8c714509c01355e46225a81a92a
dgatanis <122374677+dgatanis@users.noreply.github.com>
2024-05-12 11:33:07 -0600

    asdf

--- src/js/CurrentSeason/currentSeason.js
+++ src/js/CurrentSeason/currentSeason.js
@@ -1024,28 +1024,6 @@ function createAccordionItem(weekNumber) {
     return accordionItem;
 }
 
-function createOwnerAvatarImage(userId) { 
-
-    let user = userData.find(x => x.user_id === userId);
-    const avatarURL = user.metadata.avatar;
-    
-    if(avatarURL)
-    {
-        var img = document.createElement("img");
-        img.setAttribute('src', avatarURL);
-        img.setAttribute('class', "custom-medium-avatar");
-        img.setAttribute('data-userid', user.user_id);
-    }
-    else
-    {
-        var img = document.createElement("img");
-        img.setAttribute('src', '../src/static/images/trashcan.png');
-        img.setAttribute('class', "custom-medium-avatar");
-        img.setAttribute('data-userid', user.user_id);
-    }
-    return img;
-}
-
 function createPlayerImage(playerId) {
 
     let player = playerData.players.find(e => e.player_id === parseInt(playerId));
