f619ef3fe8f966d58dc8cfae7d86b49c5410d30e
dgatanis <122374677+dgatanis@users.noreply.github.com>
2024-05-12 09:52:23 -0600

    ewr

--- src/js/Rosters/testing.js
+++ src/js/Rosters/testing.js
@@ -3,6 +3,10 @@ import { rosterDatas} from '../util/helper.js';
 let rosterData = rosterDatas;
 console.log(rosterData);
 
+function thisIsMyTest() {
+    console.log('myTestFunc');
+}
+
 
 // let user = userData.find(x => x.user_id === '861092508472578048');
 // console.log(user.display_name);
