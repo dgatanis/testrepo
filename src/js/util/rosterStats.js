b16fddf2d24bede5e791d519d8eee76941358b3c
dgatanis <122374677+dgatanis@users.noreply.github.com>
2024-05-12 11:02:59 -0600

    asdf

--- src/js/util/rosterStats.js
+++ src/js/util/rosterStats.js
@@ -236,3 +236,22 @@ function calcPlayerAge(players) {
 
     return calculatedAge;
 }
+
+function getPlayerPointsForWeek(playerid,week) {
+
+    let matchups = matchupData[0].matchupWeeks[week];
+    let matchupsLength = Object.keys(matchups).length;
+    
+    //Iterate through each matchup and then return player points for the provided week
+    for(let i =0; i<matchupsLength; i++)
+    {
+        let matchup = matchups[i];
+        let matchupPoints = matchup.players_points;
+        if(matchupPoints[playerid] != null && matchupPoints[playerid] != undefined && matchupPoints[playerid] != NaN)
+        {
+            var round = Math.round;
+            return round(matchupPoints[playerid], 0);
+        }
+        
+    }
+}
