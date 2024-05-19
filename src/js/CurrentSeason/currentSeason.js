3ff5eb6abab965d246e02006262829ad2e4e104b
dgatanis <122374677+dgatanis@users.noreply.github.com>
2024-05-19 07:55:22 -0600

    asdf

--- src/js/CurrentSeason/currentSeason.js
+++ src/js/CurrentSeason/currentSeason.js
@@ -35,6 +35,7 @@ async function loadContents() {
         const weeklyWinnerPayout = leagueInfo.weeklyWinner;
         const dues = leagueInfo.dues;
 
+        loadPlayoffs();
         loadSeasonRankings();
         loadMatchupsList(); 
         loadBankroll(currentWeek,dues,weeklyWinnerPayout); 
