173e3b2e8ced486c674c08766383bd447cf72a6c
dgatanis <122374677+dgatanis@users.noreply.github.com>
2024-05-18 18:18:43 -0600

    asdf

--- src/js/Rosters/rosters.js
+++ src/js/Rosters/rosters.js
@@ -20,7 +20,7 @@ loadContents();
 
    
 function showNavbar (toggleId, navId, bodyId, headerId) {
-    
+
     const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId),
     bodypd = document.getElementById(bodyId),
@@ -39,25 +39,17 @@ function showNavbar (toggleId, navId, bodyId, headerId) {
             headerpd.classList.toggle('body-pd');
         });
     }
-}
-    
-    
-    showNavbar('header-toggle','nav-bar','body-pd','header');
-    
+
     /*===== LINK ACTIVE =====*/
     const linkColor = document.querySelectorAll('.nav_link');
-    
+
     function colorLink(){
     if(linkColor){
         linkColor.forEach(l=> l.classList.remove('active'));
         this.classList.add('active');
     }
     }
-    linkColor.forEach(l=> l.addEventListener('click', colorLink))
-    
-     // Your code to run since DOM is loaded and ready
-    });
-
+}
 
 //This loads the page contents dynamically
 function loadContents() {
