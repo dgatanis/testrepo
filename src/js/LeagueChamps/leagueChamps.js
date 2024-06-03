34f63e8b70dfb50726a0695c3e1b1e46aa90a814
dgatanis <122374677+dgatanis@users.noreply.github.com>
2024-06-02 20:25:27 -0600

    asdf

--- src/js/LeagueChamps/leagueChamps.js
+++ src/js/LeagueChamps/leagueChamps.js
@@ -130,12 +130,12 @@ function createLeagueChampRow(roster, user, year, matchups = null) {
 
     if(year >= 2024)
     {
-        var finals = matchups.find(x => x.roster_id === parseInt(roster.roster_id));
+        var finals = matchups.find(x => x.roster_id === parseInt(5));//matchups.find(x => x.roster_id === parseInt(roster.roster_id));
         var highScorer = highScorerInMatchupStarters(finals.starters, finals.players_points);
 
         console.log(finals);
         console.log(highScorer);
-        
+
         var playerName = document.createElement('div');
         playerName.innerText = getFullPlayerName(highScorer.player_id);
 
