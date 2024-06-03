31802f67adbb514a97893f38fef5539d20b061fc
dgatanis <122374677+dgatanis@users.noreply.github.com>
2024-06-02 20:03:04 -0600

    asdf

--- src/js/LeagueChamps/leagueChamps.js
+++ src/js/LeagueChamps/leagueChamps.js
@@ -1,19 +1,23 @@
 import { 
     rosters, 
     users, 
+    players,
     matchups, 
     playoffs, 
     allTimeLeagueIds,
     createOwnerAvatarImage, 
     getRosterStats, 
     getTeamName,
-    createNFLTeamImage 
+    createNFLTeamImage, 
+    highScorerInMatchupStarters,
+    getFullPlayerName
 } from '../util/helper.js';
 
 let userData = users;
 let rosterData = rosters;
 let matchupData = matchups;
 let playoffData = playoffs;
+let playerData = players;
 
 loadContents();
 
@@ -74,12 +78,13 @@ async function loadLeagueChamps(inauguralSeason) {
 
     for(let league of leagues.ATLeagueId)
     {
-        var rosterId = await getChampionForLeague('998356266604916736');//getChampionForLeague(league.league_id);
-        if(rosterId)
+        var playoffRound = await getChampionForLeague('998356266604916736');//getChampionForLeague(league.league_id);
+        
+        if(playoffRound)
         {
-            var roster = rosterData.find(x => x.roster_id === parseInt(rosterId));
+            var roster = rosterData.find(x => x.roster_id === parseInt(playoffRound.w));//winner of the finals
             var user = userData.find(x => x.user_id === roster.owner_id);
-    
+
             var leagueChampRow = createLeagueChampRow(roster, user, league.year);
             
             tableBody.appendChild(leagueChampRow);
@@ -99,10 +104,18 @@ async function getChampionForLeague(leagueId) {
         {
             if(playoffRound.w)
             {
-                return playoffRound.w;
+                return playoffRound;
             }
         }
     }
+}
+
+async function getFinalsMatchups(leagueId) {
+
+    const res = await fetch(`https://api.sleeper.app/v1/league/${leagueId}/matchups/17`);//last week of season  
+    const data = await res.json(); 
+
+    return data;
 }
 
 function createLeagueChampRow(roster, user, year) {
@@ -112,6 +125,19 @@ function createLeagueChampRow(roster, user, year) {
     var tdTeam = document.createElement('td');
     var teamImage = createOwnerAvatarImage(user.user_id);
     var teamName = document.createElement('div');
+
+    if(year >= 2024)
+    {
+        var matchups =  await getFinalsMatchups(league.league_id);
+        var finals = matchups.find(x => x.roster_id === parseInt(roster.roster_id));
+        var highScorer = highScorerInMatchupStarters(finals.starters, finals.players_points);
+
+
+        var playerName = document.createElement('div');
+        playerName.innerText = getFullPlayerName(highScorer.player_id);
+
+        teamName.append(playerName);
+    }
 
     teamName.setAttribute('class', 'custom-team-name');
     teamName.innerText=getTeamName(user.user_id);
