ce201e5ea26b1b72630818ff0a5982ee459ace49
dgatanis <122374677+dgatanis@users.noreply.github.com>
2024-06-23 07:14:03 -0600

    asdf

--- src/js/CurrentSeason/functions.js
+++ src/js/CurrentSeason/functions.js
@@ -69,7 +69,7 @@ async function OpenTeamRosterModal(userid,teamname) {
                 var playerNameDiv = document.createElement("div");
                 playerNameDiv.setAttribute('class', 'custom-playername-small');
                 playerNameDiv.setAttribute('style', 'margin-top:.2rem;');
-                playerNameDiv.innerText = playerName + " " + highScorerPlayers[i].points + "pts"
+                playerNameDiv.innerText = playerName + ": " + highScorerPlayers[i].points + "pts"
                  
                 highScorers[i].append(playerNameDiv);
                 highScorers[i].prepend(playerImg);
