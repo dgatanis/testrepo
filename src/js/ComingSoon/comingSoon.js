93e491e0b4fa312e09fb4af7bc1f135b6bc642b0
dgatanis <122374677+dgatanis@users.noreply.github.com>
2024-06-29 09:37:57 -0600

    asdfg

---
+++ src/js/ComingSoon/comingSoon.js
@@ -0,0 +1,11 @@
+import { 
+    setLeagueName 
+    } 
+from '../util/helper.js';
+
+loadContents();
+
+//This loads the page contents dynamically
+function loadContents() {
+    setLeagueName("footerName");
+}
