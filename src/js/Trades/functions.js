42d6416d39335a9c85ef64bde1b3ef0f7f2583c6
Don <dgatanis12@gmail.com>
2024-08-18 17:01:25 -0600

    fix pagination

--- src/js/Trades/functions.js
+++ src/js/Trades/functions.js
@@ -20,7 +20,7 @@ function openRotoWirePageDef(first_name, last_name, team){
 function showPage(pageNumber) {
     var trades = document.getElementsByClassName('custom-trades-page-'+pageNumber.toString());
     var allTrades = document.querySelectorAll('[class*=custom-transaction-row]');
-    var totalPages = Math.round(allTrades.length / 10);
+    var totalPages = Math.ceil(allTrades.length / 10);
 
     hideAllPages(pageNumber);
 
