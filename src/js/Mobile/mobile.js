779e5fcdfbf5b828cf854ed25115616a49f85133
Don <dgatanis12@gmail.com>
2024-09-29 07:41:10 -0600

    cleanup mobile display

--- src/js/Mobile/mobile.js
+++ src/js/Mobile/mobile.js
@@ -1,9 +1,22 @@
 //Custom mobile navigation 
 function toggleMobileMenu() {
-    var x = document.getElementById("mobileLinks");
-    if (x.classList.value === "custom-mobile-show") {
-        x.setAttribute('class', 'custom-mobile-hide');
+    var mobileLinks = document.getElementById("mobileLinks");
+    var leagueLogo = document.getElementById("league-logo-mobile");
+
+    if (mobileLinks.classList.value === "custom-mobile-show") {
+        mobileLinks.setAttribute('class', 'custom-mobile-hide');
+
+
     } else {
-        x.setAttribute('class', 'custom-mobile-show');
+        mobileLinks.setAttribute('class', 'custom-mobile-show');
+    }
+
+    if(leagueLogo.classList.contains("custom-fade-in"))
+    {
+        leagueLogo.classList.remove('custom-fade-in');
+    }
+    else
+    {
+        leagueLogo.classList.add('custom-fade-in');
     }
 }
