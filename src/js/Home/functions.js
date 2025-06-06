c050a8cae7556009c49e8897f91a9b1f26acc436
Don <dgatanis12@gmail.com>
2025-06-06 13:34:53 -0600

    testing

--- src/js/Home/functions.js
+++ src/js/Home/functions.js
@@ -1,9 +1,10 @@
 function openRostersPage(rosterid) {
+    var url = new URL(window.location.href.toString());
     var newURL = new URL('https://dgatanis.github.io/testrepo/web/Rosters.html');
     newURL.searchParams.append('callFunction', 'openRoster');
     newURL.searchParams.append('rosterId', rosterid);
 
-    history.replaceState(null, null, newURL.toString());
+    history.pushState(null, null, url.toString());
     history.pushState(null, null, newURL.toString());
     window.location.href = newURL;
     return;
