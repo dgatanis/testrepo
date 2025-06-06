7c9ab30367aaaccc4913c0df221377f015b0f086
Don <dgatanis12@gmail.com>
2025-06-06 13:42:03 -0600

    add back in code

--- src/js/Home/functions.js
+++ src/js/Home/functions.js
@@ -1,11 +1,9 @@
 function openRostersPage(rosterid) {
-    var url = new URL(window.location.href.toString());
-    var newURL = new URL('https://dgatanis.github.io/testrepo/web/Rosters.html');
+    var newURL = new URL('https://dgatanis.github.io/CrushCitiesFFL/web/Rosters.html');
     newURL.searchParams.append('callFunction', 'openRoster');
     newURL.searchParams.append('rosterId', rosterid);
-    
-    window.history.pushState(null, null, url.toString());
-    window.history.pushState(null, null, newURL.toString());
+
+    history.pushState({}, '', newURL);
     window.location.href = newURL;
     return;
 }
