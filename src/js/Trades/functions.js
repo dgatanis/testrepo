dfa9468e75b44c6f45c712cab9aee583a5d7ca5b
Don <dgatanis12@gmail.com>
2025-12-05 14:05:08 -0700

    save work

--- src/js/Trades/functions.js
+++ src/js/Trades/functions.js
@@ -23,74 +23,78 @@ function openRotoWirePageDef(first_name, last_name, team){
     return;
 }
 
-function showPage(pageNumber) {
-    var trades = document.getElementsByClassName('custom-trades-page-'+pageNumber.toString());
-    var allTrades = document.querySelectorAll('[class*=custom-transaction-row]');
-    var totalPages = Math.ceil(allTrades.length / 10);
-
-    hideAllPages(pageNumber);
-
-    for(let trade of trades)
-    {
-        trade.classList.remove('custom-none-display');
-    }
-
-    var paginationList = document.getElementById('trades-pagination');
-    paginationList.setAttribute('data-current-page', pageNumber);
-
-    updatePagination(pageNumber, totalPages);
-    
-}
-
-function hideAllPages(exceptPage) {
-    const elements = document.querySelectorAll('[class*="custom-trades-page-"]');
-
-    for(let element of elements)
-    {
-        if(!element.classList.contains('custom-trades-page-'+exceptPage.toString()))
-        {
-            element.classList.add('custom-none-display');
-        }
-    }
-}
-
 function togglePage(nextPrev) {
-    var currentPage = document.getElementById('trades-pagination').getAttribute('data-current-page');
-    
-    if(nextPrev == 'N')
-    {
-        var pageNum = parseInt(currentPage)+1
-
-        if(document.getElementsByClassName('custom-trades-page-'+pageNum.toString()).length >= 1)
-        {
-            showPage(pageNum);
-            var pageNumberLinks = document.getElementsByClassName('page-number')
-            pageNumberLinks[parseInt(currentPage)].focus();
-        }
-        
-    }
-    else if (nextPrev == 'P')
-    {
-        var pageNum = parseInt(currentPage)-1
-
-        if(document.getElementsByClassName('custom-trades-page-'+pageNum.toString()).length >= 1)
-        {
-            showPage(pageNum);
-            var pageNumberLinks = document.getElementsByClassName('page-number')
-            pageNumberLinks[parseInt(currentPage)-2].focus();
-        }
+    var currentPage = parseInt(document.getElementById('trades-pagination').getAttribute('data-current-page'));
+    var allTrades = document.querySelectorAll('[class*="custom-transaction-row"]');
+    var allShownTrades = document.querySelectorAll(`[data-shown='true']`);
+    var firstShownTrade = allShownTrades[0].getAttribute("data-transaction-date");
+    var finalShownTrade = allShownTrades[allShownTrades.length - 1].getAttribute("data-transaction-date");
+    var beginIndex = parseInt(Array.from(allTrades).findIndex(trade => parseInt(trade.getAttribute("data-transaction-date")) === parseInt(firstShownTrade)));
+    var endIndex = parseInt(Array.from(allTrades).findIndex(trade => parseInt(trade.getAttribute("data-transaction-date")) === parseInt(finalShownTrade)));
+    var totalPages = Math.ceil(allTrades.length / 10);
+ 
+    for (var trade of allShownTrades) {
+        trade.setAttribute("data-shown", "false");
+        trade.classList.add("custom-none-display");
     }
     
+    for (const [index, trade] of allTrades.entries()) {
+        if (nextPrev == 'N') {
+            var remainder = endIndex % 10;
+            if (   index == endIndex + 1 
+                || (index > endIndex && index <= endIndex + 10) 
+                || (endIndex == allTrades.length - 1 && index <= endIndex && index >= endIndex - remainder)) { //If its final trade in index then get the last remaining trades until a number divisible by 10
+                trade.setAttribute("data-shown", "true");
+                trade.classList.remove("custom-none-display");
+            }
+            else {
+                trade.setAttribute("data-shown", "false");
+                trade.classList.add("custom-none-display");
+            }
+            
+        }
+        else if (nextPrev == 'P') {
+            if (index == beginIndex - 1 || (index < beginIndex && index >= beginIndex - 10) || (beginIndex == 0 && index <= 9)) {
+                trade.setAttribute("data-shown", "true");
+                trade.classList.remove("custom-none-display");
+            }
+            else {
+                trade.setAttribute("data-shown", "false");
+                trade.classList.add("custom-none-display");
+            }
+        }
+    }
+    if(nextPrev == 'N') { //Set current page
+        if(currentPage + 1 > totalPages) {
+            currentPage = currentPage;
+        }
+        else {
+            currentPage = currentPage + 1;
+        }
+    }
+    else if(nextPrev == 'P') {
+        if(currentPage - 1 <= 0) {
+            currentPage = currentPage;
+        } 
+        else {
+            currentPage = currentPage - 1;
+        }
+    }
+
+    document.getElementById('trades-pagination').setAttribute('data-current-page', currentPage);
+    updatePagination(currentPage, totalPages);
 }
 
 function updatePagination(currentPage, totalPages) {
     const pagination = document.getElementById('trades-pagination');
     const pageItems = pagination.getElementsByClassName('page-number');
     const ellipsis = pagination.querySelectorAll('.ellipsis');
-    
+    var currentPageItem = pageItems[currentPage-1];
+
     // Hide all pages initially
     for (let item of pageItems) {
         item.classList.add('custom-none-display');
+        item.classList.remove("custom-active");
     }
     let start = Math.max(1, currentPage - 2);  // Show 2 pages before the current one
     let end = Math.min(totalPages, parseInt(currentPage) + 2);  // Show 2 pages after the current one
@@ -121,4 +125,69 @@ function updatePagination(currentPage, totalPages) {
     for (let i = start - 1; i < end; i++) {
         pageItems[i].classList.remove('custom-none-display');
     }
+
+    currentPageItem.classList.add("custom-active");
+}
+
+async function filterTrades() {
+    const helper = await import('../util/helper.js');
+    var allTrades = document.querySelectorAll(`[data-transaction-date]`);
+    var teamDropdown = document.getElementById("custom-team-selector-input");
+    var startDate = document.getElementById("startDate");
+    var endDate = document.getElementById("endDate");
+    var selectedUserId = teamDropdown.innerText == '' ? null : helper.getUserByName(teamDropdown.innerText);
+    var selectedStartDate = startDate.value == '' ? '1/1/1901' : startDate.value;
+    var selectedEndDate = endDate.value == '' ? '1/1/2099' : endDate.value;
+
+    if (validateFilters(selectedUserId, selectedStartDate, selectedEndDate) == 1) return;
+
+    for (var trade of allTrades) {
+        var date = parseInt(trade.getAttribute("data-transaction-date"));
+
+        if (parseInt(new Date(selectedStartDate).getTime()) <= date && date <= parseInt(new Date(selectedEndDate).getTime())) {
+
+            if (selectedUserId != null) {
+                var user = trade.querySelectorAll(`[data-userid='${selectedUserId}']`);
+
+                if (user.length >= 1) {
+                    trade.classList = 'custom-transaction-row custom-trades-page-1';
+                }
+                else {
+                    if (!trade.classList.contains("custom-none-display")) {
+                        trade.classList.add("custom-none-display");
+                    }
+                }
+            }
+            else {
+                trade.classList = 'custom-transaction-row custom-trades-page-1';
+            }
+        }
+        else {
+            if (!trade.classList.contains("custom-none-display")) {
+                trade.classList.add("custom-none-display")
+            }
+        }
+    }
+
+    document.getElementById("trade-page-navigation-container").classList.add("custom-none-display");
+
+}
+
+function validateFilters(selectedUserId, selectedStartDate, selectedEndDate) {
+    const startDate = new Date(selectedStartDate);
+    const endDate = new Date(selectedEndDate);
+
+    if (endDate.getTime() < startDate.getTime()) {
+        window.alert("From Date cannot precede To Date.");
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+
+function createFilterDescription(selectedUserId, selectedStartDate, selectedEndDate) {
+    var descriptionContainer = document.createElement("div");
+    var teamFilter = document.createElement("div");
+    var dateFilterContainer
 }
